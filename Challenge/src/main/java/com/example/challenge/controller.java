508cbd3a006e8f3f55140e50b05d622781a9dcef
Matthew Orwin <73421826+s3782972mattheworwin@users.noreply.github.com>
2022-09-09 03:14:08 +1000

    controller features added

--- Challenge/src/main/java/com/example/challenge/controller.java
+++ Challenge/src/main/java/com/example/challenge/controller.java
@@ -1,4 +1,53 @@
 package com.example.challenge;
 
-public class controller {
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class controller {
+    private final ItemRepository repository;
+
+    controller(ItemRepository repository){
+        this.repository = repository;
+    }
+
+    @GetMapping("/items")
+    List<Item> all(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/items/{id}")
+    Item one(@PathVariable Long id){
+        return repository.findById(id)
+                .orElseThrow(() -> ItemNotFoundExcpetion(id));
+    }
+
+    @PutMapping("/items/{id}")
+    Item replaceItem(@RequestBody Item newItem, @PathVariable Long id){
+        return repository.findById(id)
+                .map(item -> {
+                    item.setName(newItem.getName());
+                    item.setDesc(newItem.getDesc());
+                    item.setId(newItem.getId());
+                    item.setPrice(newItem.getPrice());
+                    return repository.save(item);
+                })
+                .orElseGet(() -> {
+                    newItem.setId(id.toString());
+                    return repository.save(newItem);
+                });
+    }
+
+    @DeleteMapping("/employees/{id}")
+    void deleteItem(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+
 }
